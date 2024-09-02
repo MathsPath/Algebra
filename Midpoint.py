109ee71f83d3ee8531113dec4441865f43e1e502
MathsPath <dgh38155@gmail.com>
2024-09-01 22:24:46 -0400

    Update Midpoint.py

--- Midpoint.py
+++ Midpoint.py
@@ -5,8 +5,8 @@ a = input("Input A coordinate (separate with ','): ").split(',')
 b = input("Input B coordinate (separate with ','): ").split(',')
 
 def midpoint(point1, point2):
-    x = ( point1[0] + point2[0] ) / 2
-    y = ( point1[-1] + point2[-1] ) / 2
+    x = ( float(point1[0]) + float(point2[0]) ) / 2
+    y = ( float(point1[-1]) + float(point2[-1]) ) / 2
     return x, y
 
 print(midpoint(a,b))
