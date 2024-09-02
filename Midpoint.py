339d4bb7deac47fc1a2de2b176c1429545671675
MathsPath <dgh38155@gmail.com>
2024-09-01 22:20:09 -0400

    Midpoint.py

--- Midpoint.py
+++ Midpoint.py
@@ -1,12 +1,12 @@
 # Midpoint of a line on coordinate plane
 
 # Points of a line
-a_point = (-1,-1)
-b_point = (4, -3)
+a = input("Input A coordinate (separate with ','): ").split(',')
+b = input("Input B coordinate (separate with ','): ").split(',')
 
 def midpoint(point1, point2):
     x = ( point1[0] + point2[0] ) / 2
     y = ( point1[-1] + point2[-1] ) / 2
     return x, y
 
-print(midpoint(a_point,b_point))
+print(midpoint(a,b))
